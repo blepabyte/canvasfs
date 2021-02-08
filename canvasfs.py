ccc7292f7a87e8b86899f80f6b2926290cbf6396
blepabyte <74576829+blepabyte@users.noreply.github.com>
2021-02-08 16:56:43 +1300

    An intermediate state before trashing all the old code

--- canvasfs.py
+++ canvasfs.py
@@ -2,53 +2,77 @@ import json
 import argparse
 import functools
 from time import perf_counter
-import os, sys, stat, errno
+import os
+import sys
+import stat
+import errno
+import pathlib
 from datetime import datetime
 import trio
 import pyfuse3
+import canvasapi
 from canvasapi import Canvas
 from canvasapi.course import Course
 from canvasapi.file import File
 from canvasapi.folder import Folder
 import logging
 from loguru import logger
+import weakref
+
+from fs import Node, OpenNode, Subdirectory
+from fs import bijection_forwards, bijection_backwards, default_inode_entry, datetime_to_fstime, fuse_assert
 
 NANOSECONDS = 1e9
 CACHE_LOCATION = "/mnt/storage/.canvas_fs"
 
-
-def default_inode_entry(inode) -> pyfuse3.EntryAttributes:
-    """
-    Caller must set `st_mode` and `st_size` on returned object before it is considered valid. Name?
-    """
-    entry = pyfuse3.EntryAttributes()
-
-    # Seconds --> nanoseconds
-    stamp = datetime.now().timestamp() * NANOSECONDS
-
-    entry.st_atime_ns = stamp # accessed
-    entry.st_ctime_ns = stamp # created
-    entry.st_mtime_ns = stamp # modified
-    entry.st_gid = os.getgid()
-    entry.st_uid = os.getuid()
-    entry.st_ino = inode
-
-    # entry.attr_timeout = 10000
-    # entry.entry_timeout = 10000
-
-    return entry
+PYFUSE_ROOT_ATTR = default_inode_entry(pyfuse3.ROOT_INODE)
+PYFUSE_ROOT_ATTR.st_mode = (stat.S_IFDIR | 0o755)
+PYFUSE_ROOT_ATTR.st_size = 0
 
 
-def default_folder_entry(inode) -> pyfuse3.EntryAttributes:
-    folder_entry = default_inode_entry(inode)
-    folder_entry.st_mode = (stat.S_IFDIR | 0o755)
-    folder_entry.st_size = 0
-    return folder_entry
+class ConfigError(Exception):
+    pass
 
 
-def get_cache_size() -> int:
-    """Returns total size of cache in bytes"""
-    return sum(os.stat(f"{CACHE_LOCATION}/{node}").st_size for node in os.listdir(CACHE_LOCATION))
+def config() -> dict:
+    if config.CONFIG is not None:
+        return config.CONFIG
+
+    # Load config file for the first time
+    if len(sys.argv) == 1:
+        config_location = "config.json"
+    elif len(sys.argv) == 2:
+        config_location = sys.argv[1]
+    else:
+        raise ConfigError(
+            "Too many command-line arguments: Expecting either none or path to a configuration file")
+
+    with open(config_location) as f:
+        config.CONFIG = json.load(f)
+
+    return config()
+
+
+config.CONFIG = None
+
+
+def canvas() -> canvasapi.canvas.Canvas:
+    if canvas.CANVAS is not None:
+        return canvas.CANVAS
+
+    token = config()["token"]
+    domain = config()["domain"]
+    canvas.CANVAS = Canvas(domain, token)
+    return canvas()
+
+
+canvas.CANVAS = None
+
+
+def cache_size() -> int:
+    """Computes total size of cache in bytes"""
+    cache_location = config()["cache_dir"]
+    return sum(os.stat(f"{cache_location}/{node}").st_size for node in os.listdir(cache_location))
 
 
 # class DoubleDict:
@@ -126,7 +150,8 @@ class FileAccessWrapper:
 
             ts = perf_counter()
             contents = self.file_contents(fh)
-            logger.success(f"Fetched {self.inodes[fh].filename} from Canvas API in {perf_counter() - ts:.3f}s")
+            logger.success(
+                f"Fetched {self.inodes[fh].filename} from Canvas API in {perf_counter() - ts:.3f}s")
 
             with open(f"{CACHE_LOCATION}/{fh}", "wb") as f:
                 f.write(contents)
@@ -168,11 +193,13 @@ class FileAccessWrapper:
         logger.trace(f"directory listing: {self.name} @ {start_id}")
 
         # TODO: Sub-directory support
-        file_order = list(filter(lambda f: isinstance(f, File), self.inodes.values()))
+        file_order = list(
+            filter(lambda f: isinstance(f, File), self.inodes.values()))
         file_order.sort(key=lambda f: f.id)
 
         if start_id >= len(file_order):
-            logger.debug(f"start_id {start_id} is past end of directory listing")
+            logger.debug(
+                f"start_id {start_id} is past end of directory listing")
             # raise pyfuse3.FUSEError(errno.ENOENT)
             return
 
@@ -180,13 +207,258 @@ class FileAccessWrapper:
         it = enumerate(file_order)
         for _ in range(start_id):
             next(it)
-        
+
         # Return next node ID in reply directly rather than arbitrary count
         for num, file in it:
             # Expects bytes as name
             logger.trace(f"[{num}] {file.filename}")
             if not pyfuse3.readdir_reply(token, file.filename.encode('utf-8'), self.getattr(file.id), num + 1):
                 break
+
+
+class NullFS:
+    def inode_belongs(self, _inode) -> bool:
+        return False
+
+    async def getattr(self, inode, ctx=None):
+        raise pyfuse3.FUSEError(errno.ENOENT)
+
+    async def lookup(self, parent_inode, name, ctx=None):
+        raise pyfuse3.FUSEError(errno.ENOENT)
+
+    async def opendir(self, inode, ctx):
+        raise pyfuse3.FUSEError(errno.ENOENT)
+
+    async def open(self, inode, flags, ctx):
+        raise pyfuse3.FUSEError(errno.ENOENT)
+
+
+class SubFS:
+    """
+    Each SubFS is responsible only for the inodes for which its `inode_belongs` method returns True. It follows that inodes should be unique between different SubFS instances. 
+
+    The following methods on inodes are directly passed through from the root FS instance and should be passed to its child if the target inode does not belong. 
+    - getattr
+    - lookup
+    - opendir
+    - open
+    """
+
+    def __init__(self, init_list, num=2):
+
+        assert len(init_list) > 0
+        this_course, *rest = init_list
+        self.id = this_course["id"]
+        self.number = num
+
+        self.cache_root = pathlib.Path(config()["cache_dir"]) / str(self.id)
+        self.cache_root.mkdir(parents=True, exist_ok=True)
+
+        self.root_inode = bijection_forwards(self.number, 0)
+
+        self.course = canvas().get_course(self.id)
+        self.has_subdirectories = this_course.get("subdirectories", False)
+
+        self.name = this_course["name"] if "name" in this_course else self.course.course_code
+
+        if rest:
+            self.child = SubFS(rest, num + 1)
+        else:
+            self.child = NullFS()
+
+        # if not has_subdirectories this is just the root inode -> all files
+        self.subdirectories: {int, Subdirectory} = {}  # { folder_inode -> Subdirectory <: Node }
+        self.files: {int, Node} = {}  # { file_inode -> FileNode <: Node }
+
+    def inode_belongs(self, inode) -> bool:
+        return inode in self.files or inode in self.subdirectories or inode == self.root_inode
+
+    async def poll(self):
+        while True:
+            await trio.sleep(60 * 10)
+            await self.build()
+
+    async def build(self):
+        all_files = [Node(bijection_forwards(self.number, f.id), f) for f in self.course.get_files()]
+        all_folders = [Node(bijection_forwards(self.number, f.id), f) for f in self.course.get_folders()]
+
+        self.files = all_files
+        self.subdirectories = {
+            folder_node.inode: Subdirectory(
+                list(filter(lambda n: n.parent() == folder_node.inode, all_files + all_folders))
+            )
+            for folder_node in all_folders
+        }
+
+    # Passthrough methods
+
+    async def getattr(self, inode, ctx=None):
+        if not self.inode_belongs(inode):
+            return await self.child.getattr(inode, ctx)
+
+        if inode == self.root_inode:
+            entry = default_inode_entry(inode)
+            entry.st_mode = (stat.S_IFDIR | 0o755)
+            entry.st_size = 0
+
+            entry.st_ctime_ns = datetime_to_fstime(self.course.created_at_date)
+            # TODO: mtime should be set to most recent modification out of all contained files
+            return entry
+
+        if inode in self.files:
+            return self.files[inode].attributes()
+
+        if inode in self.subdirectories:
+            return self.subdirectories[inode].attributes()
+
+        assert False, inode
+
+    async def lookup(self, parent_inode, name, ctx=None):
+        found = self.subdirectories[parent_inode].with_name(name.decode('utf-8'))
+        fuse_assert(found is not None)
+        return found.attributes()
+
+    async def opendir(self, inode, ctx) -> OpenNode:
+        fuse_assert(inode in self.subdirectories)
+        # TODO: Return `weakref` as it's possible Subdirectory will be dumped into garbage when rebuild called
+        return self.subdirectories[inode]
+
+    async def open(self, inode, flags, ctx) -> OpenNode:
+        fuse_assert(inode not in self.files)
+        return self.files[inode]
+
+    # Methods on file handles
+
+    async def readdir(self, fh, start_id, token):
+        for entry in self.subdirectories[fh].list_files(start_id):
+            if not pyfuse3.readdir_reply(token, entry.name.encode('utf-8'), entry.attributes(), entry.inode + 1):
+                return
+
+    async def read(self, fh, off, size):
+        # try to return read from cache
+        # todo: check if modified should happen during build() not here
+        cache_path = self.cache_root / str(bijection_backwards(fh)[1])
+        if cache_path.exists():
+            with open(cache_path, "rb") as f:
+                f.seek(off)
+                return f.read(size)
+
+        # not found in cache so fetch from network
+        def fetch():
+            ts = perf_counter()
+            file = self.files[fh].obj
+            data = file.get_contents(binary=True)
+            logger.info(f"Fetched '{file.filename}' over network in {perf_counter() - ts:.3f}s")
+            return data
+
+        with open(cache_path, "wb") as f:
+            f.write(await trio.to_thread.run_sync(fetch))
+        # no point rewriting the cache read from above. also prevents indexing errors
+        return await self.read(fh, off, size)
+
+
+class FS(pyfuse3.Operations):
+    supports_dot_lookup = False
+
+    def __init__(self, courses_config):
+        super().__init__()
+
+        self.root_child = SubFS(courses_config)
+        self.subsystem = {}  # { course_num -> SubFS }
+        self.open_handles = {}  # { inode -> OpenNode }
+
+        cur_child = self.root_child
+        while not isinstance(cur_child, NullFS):
+            self.subsystem[cur_child.number] = cur_child
+            cur_child = cur_child.child
+
+        self.total_bytes_read = 0
+
+    @staticmethod
+    def create():
+        """
+        Sets up an FS instance. If no courses are provided in the config this may take a while, as each course needs to be checked for an accessible Files tab synchronously
+        # TODO: Parallelise checks via async threads
+        """
+
+        # TODO: In the case of non-accessible files, an alternative can be scraping all the module pages for links to files and just dumping them? (override build?)
+        def has_accessible_files(course_config) -> bool:
+            target_course = canvas().get_course(course_config["id"])
+            try:
+                _f = list(target_course.get_files())
+            except canvasapi.exceptions.Unauthorized:
+                logger.warning(f"The course '{target_course.name}' does not have an accessible files tab")
+                return False
+            return True
+
+        if "courses" in config():
+            courses_config = config()["courses"]
+        else:
+            # User did not specify which courses to mount, so try all of them.
+            courses_config = []
+            active_courses = list(canvas().get_courses())
+            for c in active_courses:
+                courses_config.append({
+                    "id": c.id
+                })
+
+        return FS(list(filter(has_accessible_files, courses_config)))
+
+    # Passthrough methods
+
+    async def getattr(self, inode, ctx=None):
+        if inode == pyfuse3.ROOT_INODE:
+            return PYFUSE_ROOT_ATTR
+
+        return await self.root_child.getattr(inode, ctx)
+
+    async def lookup(self, parent_inode, name, ctx=None):
+        if parent_inode == pyfuse3.ROOT_INODE:
+            for sub in self.subsystem.values():
+                if name == sub.name:
+                    return await sub.getattr(sub.root_inode, ctx)
+            raise pyfuse3.FUSEError(errno.ENOENT)
+
+        return await self.root_child.lookup(parent_inode, name, ctx)
+
+    async def opendir(self, inode, ctx):
+        if inode == pyfuse3.ROOT_INODE:
+            return inode
+
+        handle = await self.root_child.opendir(inode, ctx)
+        self.open_handles[inode] = handle
+        assert handle.is_folder()
+        return handle.inode
+
+    async def open(self, inode, flags, ctx):
+        handle = await self.root_child.open(inode, flags, ctx)
+        self.open_handles[inode] = handle
+        assert handle.is_file()
+        return pyfuse3.FileInfo(fh=handle.inode)
+
+    # Methods on file handles
+
+    async def releasedir(self, fh):
+        self.open_handles.pop(fh)
+
+    async def release(self, fh):
+        self.open_handles.pop(fh)
+
+    async def readdir(self, fh, start_id, token):
+        if fh == pyfuse3.ROOT_INODE:
+            for num, sub in self.subsystem.items():
+                if num < start_id:
+                    continue
+                if not pyfuse3.readdir_reply(token, sub.name.encode('utf-8'), await sub.getattr(sub.root_inode),
+                                             num + 1):
+                    return
+
+        return await self.subsystem[self.open_handles[fh].parent_course_num].readdir(fh, start_id, token)
+
+    async def read(self, fh, off, size):
+        out = await self.subsystem[self.open_handles[fh].parent_course_num].read(fh, off, size)
+        self.total_bytes_read += len(out)
+        return out
 
 
 """
@@ -196,7 +468,9 @@ IDEAS/WIP
 
 - Recursively initialise wrappers with each pointing to "remainder" node, with a final node that just NOPs. This is elegant, and not too inefficient since # courses will be low, but for lookup calls might add non-negligible overhead... Maybe a mix of both approaches depending on the method being called? 
 """
-class FS(pyfuse3.Operations):
+
+
+class CanvasFS(pyfuse3.Operations):
     def __init__(self, course_file_wrappers: {str: FileAccessWrapper}):
 
         # pass folder name for course?
@@ -239,7 +513,8 @@ class FS(pyfuse3.Operations):
         if found is not None:
             return found
         else:
-            logger.error(f"Tried to find inode {inode} in sub-courses but it does not exist")
+            logger.error(
+                f"Tried to find inode {inode} in sub-courses but it does not exist")
             raise pyfuse3.FUSEError(errno.ENOENT)
 
     """
@@ -317,8 +592,9 @@ class FS(pyfuse3.Operations):
             return
 
         _ = self.get_course_with_inode(fh)
-        raise NotImplementedError("Sub-directories within course folders are not supported yet!")
-    
+        raise NotImplementedError(
+            "Sub-directories within course folders are not supported yet!")
+
     async def open(self, inode, flags, ctx):
         # Check `inode` exists
         _ = self.get_course_with_inode(inode)
@@ -352,8 +628,9 @@ class CanvasFSSetup:
         # TODO: shared references to wrappers
         access_wrappers = []
 
-        filesystem = FS({
-            course_name: FileAccessWrapper(course_name, canvas_obj.get_course(course_id))
+        filesystem = CanvasFS({
+            course_name: FileAccessWrapper(
+                course_name, canvas_obj.get_course(course_id))
             for course_id, course_name in self.courses
         })
 
@@ -387,7 +664,7 @@ class CanvasFSSetup:
 
         print(f"""
 Statistics:
-Current cache size: {get_cache_size() / 1e9:.3f} GB
+Current cache size: {cache_size() / 1e9:.3f} GB
 Total bytes read: <NOT YET SUPPORTED>
 """)
 
@@ -406,7 +683,8 @@ class InterceptHandler(logging.Handler):
             frame = frame.f_back
             depth += 1
 
-        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+        logger.opt(depth=depth, exception=record.exc_info).log(
+            level, record.getMessage())
 
 
 if __name__ == "__main__":
@@ -422,6 +700,10 @@ if __name__ == "__main__":
     ERROR 	    40 	    logger.error()
     CRITICAL 	50 	    logger.critical()
     """
+
+    fs = FS.create()
+
+    exit()
 
     par = argparse.ArgumentParser()
     par.add_argument("mount_point", type=str)
@@ -476,7 +758,7 @@ class InvalidConfig(Exception):
 
 
 def start_from_config(config: dict):
-    get = lambda k, e: InvalidConfig.nonempty(config.get(k), e)
+    def get(k, e): return InvalidConfig.nonempty(config.get(k), e)
 
     """
     While the recommended way to access the Canvas API is via an OAuth2 generated token, 
---
+++ fs.py
@@ -0,0 +1,139 @@
+import errno, os, stat
+from datetime import datetime
+
+import canvasapi
+import pyfuse3
+
+NANOSECONDS = 1_000_000_000
+INT64_MAX = 1 << 64 - 1
+
+
+# These functions used to create an integer `inode` from a course number and a file id
+
+
+def bijection_forwards(i, j) -> int:
+    """
+    Some bijection f: N x N -> N. No guarantee that this will be consistent between versions. 
+    """
+    assert 0 <= i < (1 << 15)  # surely a maximum of 32_000 courses is enough
+    assert 0 <= j < (1 << 48)  # quite likely file ids will stay below 281_474_976_710_656
+    return i * (1 << 48) + j
+
+
+def bijection_backwards(n) -> (int, int):
+    """
+    Inverse of `bijection_forwards`
+    """
+    return divmod(n, 1 << 48)
+
+
+def datetime_to_fstime(t: datetime) -> int:
+    return int(t.timestamp() * NANOSECONDS)
+
+
+def default_inode_entry(inode) -> pyfuse3.EntryAttributes:
+    """
+    Caller must set `st_mode` and `st_size` on returned object before it is considered valid. Name?
+    """
+    entry = pyfuse3.EntryAttributes()
+
+    # seconds --> nanoseconds
+    stamp = datetime_to_fstime(datetime.now())
+
+    entry.st_atime_ns = stamp  # accessed
+    entry.st_ctime_ns = stamp  # created
+    entry.st_mtime_ns = stamp  # modified
+    entry.st_gid = os.getgid()
+    entry.st_uid = os.getuid()
+    entry.st_ino = inode
+
+    # entry.attr_timeout = 10000
+    # entry.entry_timeout = 10000
+
+    return entry
+
+
+def fuse_assert(test: bool):
+    """
+    Will throw the FUSE equivalent of a FileNotFoundError if `test` is falsey.
+    """
+    if not test:
+        raise pyfuse3.FUSEError(errno.ENOENT)
+
+
+class Node:
+    def __init__(self, inode, obj):
+        # TODO: Set parent course num correctly during build()
+        self.parent_course_num = 0
+        self.inode = inode
+        self.obj = obj
+
+        if self.is_file():
+            self.name = self.obj.filename
+        elif self.is_folder():
+            self.name = self.obj.name
+        else:
+            assert False, self.obj
+
+    def is_file(self) -> bool:
+        return isinstance(self.obj, canvasapi.file.File)
+
+    def is_folder(self) -> bool:
+        return isinstance(self.obj, canvasapi.folder.Folder)
+
+    def parent(self):
+        if self.is_file():
+            return self.obj.folder_id
+        if self.is_folder():
+            return self.obj.parent_folder_id
+
+        assert False, self.obj
+
+    def attributes(self) -> pyfuse3.EntryAttributes:
+        if self.is_file():
+            file_attr = default_inode_entry(self.inode)
+            file_attr.st_mode = stat.S_IFREG | 0o644
+            file_attr.st_size = self.obj.size
+            file_attr.st_ctime_ns = datetime_to_fstime(self.obj.created_at_date)
+            file_attr.st_mtime_ns = datetime_to_fstime(self.obj.modified_at_date)
+            return file_attr
+
+        if self.is_folder():
+            folder_attr = default_inode_entry(self.inode)
+            folder_attr.st_mode = stat.S_IFDIR | 0o755
+            folder_attr.st_size = 0
+            folder_attr.st_mtime_ns = datetime_to_fstime(self.obj.updated_at_date)
+            return folder_attr
+
+        assert False, self.obj
+
+
+class OpenNode(Node):
+    ...
+
+
+class Subdirectory(Node):
+    def __init__(self, contents: [Node]):
+        """
+        The folder nodes in `contents` **could** implement Subdirectory but they don't - otherwise we would have a tree of objects stored
+        """
+        self.contents = contents
+
+    def with_name(self, lookup_name: str) -> Node:
+        # Try to avoid past issue where names were in bytes so all lookups failed
+        assert type(lookup_name) == str
+
+        # TODO: Proper O(1) lookup
+        for f in self.contents:
+            if f.name == lookup_name:
+                return f
+
+    def list_files(self, lower_bound_inode=0):
+        """
+        An iterator, in ascending order of inode, over all entries in `self.contents` with inode greater than the given `lower_bound`
+        # TODO: [Faster listing](https://github.com/grantjenks/python-sortedcontainers)
+        """
+        for node in sorted(self.contents, key=lambda n: n.inode):
+            if node < lower_bound_inode:
+                continue
+            yield node
